1f4cd41e8218a3468bad07abc6c634d6b84d9f27
andrey6446 <andrey6446@yandex.ru>
2024-04-25 15:17:12 +0300

    feat:middlewares:removeHeaders

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ require('@babel/register');
 
 const express = require('express');
 const ssr = require('./middleware/ssr')
+const removeHeaders = require('./middleware/removeHeaders')
 
 const app = express();
 app.use(ssr)
+app.use(removeHeaders)
 app.listen(3000, () => { console.log('Server started'); });
