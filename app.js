376c800628277c5503db6fedc3863939cddb4a27
andrey6446 <andrey6446@yandex.ru>
2024-04-25 15:14:30 +0300

    project init

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 require('@babel/register');
 
 const express = require('express');
+const ssr = require('./middleware/ssr')
 
 const app = express();
-
+app.use(ssr)
 app.listen(3000, () => { console.log('Server started'); });
