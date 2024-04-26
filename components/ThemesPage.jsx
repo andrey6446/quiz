4d8a94d9039e819b2922daa95c145c25d5ea4e48
andrey6446 <andrey6446@yandex.ru>
2024-04-26 15:45:06 +0300

    fix:styles

--- components/ThemesPage.jsx
+++ components/ThemesPage.jsx
@@ -2,11 +2,15 @@ const React = require('react');
 const Layout = require('./Layout');
 const ThemeCard = require('./ThemeCard');
 
-function ThemesPage({ title, themes }) {
+function ThemesPage({ title, themes, score }) {
   return (
     <Layout title={title}>
       <div className="themeStart">
         <p>Выберите тему</p>
+        <p>
+          Ваши баллы:
+          {score}
+        </p>
         <img src="/image/hat.svg" alt="Hat" />
       </div>
       <div className="themes">
