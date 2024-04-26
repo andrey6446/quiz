3b8f77883a793352a693e8c0aa21fdb16955ddb6
andrey6446 <andrey6446@yandex.ru>
2024-04-26 13:55:30 +0300

    feat:styles:MainPage1

--- components/MainPage.jsx
+++ components/MainPage.jsx
@@ -4,11 +4,18 @@ const Layout = require('./Layout');
 function MainPage({ title }) {
   return (
     <Layout title={title}>
-      <button>
-        <a href="/themes">
-          <p>Начать игру</p>
-        </a>
-      </button>
+      <div className="mainPage">
+        <img src="/image/bg-main-1.svg" alt="" className="img-bg-1" />
+        <div className="main-description">
+          <h1>Поднатужились</h1>
+          <h2>Андрей и Катя</h2>
+          <button className="btn-start">
+            <a href="/themes">
+              <p>Начать игру</p>
+            </a>
+          </button>
+        </div>
+      </div>
     </Layout>
   );
 }
---
+++ components/NavBar.jsx
@@ -0,0 +1,12 @@
+const React = require('react');
+
+function Layout() {
+  return (
+    <>
+      <img src="/image/logo.svg" alt="logo" className="logo" />
+      <hr />
+    </>
+  );
+}
+
+module.exports = Layout;
