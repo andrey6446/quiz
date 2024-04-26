503d7b0b4fe9d19e39a86fc5a3d75f8770a2e8e0
PavlovaKate <kv.pavlova@mail.ru>
2024-04-26 10:47:49 +0300

    feat: add photos to theme 2

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,10 +1,11 @@
-const React = require('react');
+const React = require("react");
 
 function Layout({ title, children }) {
   return (
     <html lang="ru">
       <head>
         <title>{title}</title>
+
         <link rel="stylesheet" href="/styles/style.css" />
         <script defer src="/scripts/script.js" />
       </head>
